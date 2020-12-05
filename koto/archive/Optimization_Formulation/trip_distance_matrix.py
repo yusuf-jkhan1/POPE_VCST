41b49dd5e8b2096bdbe8d6324cca35924e2898e8
yusuf-jkhan1 <73005876+yusuf-jkhan1@users.noreply.github.com>
2020-12-05 01:15:10 -0500

    Update trip_distance_matrix.py

--- koto/archive/Optimization_Formulation/trip_distance_matrix.py
+++ koto/archive/Optimization_Formulation/trip_distance_matrix.py
@@ -1,6 +1,6 @@
 import googlemaps
 
 def distance_calc(origin_lat, origin_long, dest_lat, dest_long, mode = 'driving'):
-    gmaps = googlemaps.Client(key='AIzaSyDclNSgG3YvJFNvd0aPjKc2rNBIg5U5Jy0') #TODO replace by reading from config
+    gmaps = googlemaps.Client(key='') #TODO replace by reading from config
     distance = gmaps.distance_matrix([str(origin_lat) + " " + str(origin_long)], [str(dest_lat) + " " + str(dest_long)], mode=mode)['rows'][0]['elements'][0]
     return distance['distance']['value']
